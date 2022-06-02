39337dc9599afe33b8666cf26535ef90377e6a59
rlagmltn <gmltn2015@naver.com>
2022-06-02 20:19:28 +0900

    ReNameScene SampleScene Edit WeaponSet

--- Assets/Scripts/WeaponSet.cs
+++ Assets/Scripts/WeaponSet.cs
@@ -14,33 +14,32 @@ public enum WeaponKind
 }
 public class WeaponSet : MonoBehaviour
 {
-
     private void Update()
     {
         string input = Input.inputString;
         switch (input)
         {
             case "1":
-                SetWeapon(1);
+                SetWeapon(WeaponKind.SWORD);
                 break;
             case "2":
-                SetWeapon(2);
+                SetWeapon(WeaponKind.RIFLE);
                 break;
             case "3":
-                SetWeapon(3);
+                SetWeapon(WeaponKind.SNIPER);
                 break;
             case "4":
-                SetWeapon(4);
+                SetWeapon(WeaponKind.SHOTGUN);
                 break;
             case "5":
-                SetWeapon(5);
+                SetWeapon(WeaponKind.GRANADE);
                 break;
             default:
                 return;
         }
     }
-    void SetWeapon(int _keyValue)
+    void SetWeapon(WeaponKind WK)
     {
-
+        
     }
 }
