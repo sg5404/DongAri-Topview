fb49d6eb0da1a8612e3fb0b088a76cd945bd6f0b
zmflqhaos <zmflqhaos123@gmail.com>
2022-06-02 19:45:17 +0900

    Fix Scripts

--- Assets/Scripts/WeaponSet.cs
+++ Assets/Scripts/WeaponSet.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public enum WeaponKind
 {
-    SHIELD,
-    SWORD,
-    BOW,
-    WAND,
+    SWORD = 0,
+    RIFLE,
+    SNIPER,
+    SHOTGUN,
+    GRANADE
 }
-
 public class WeaponSet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        string input = Input.inputString;
+        switch (input)
+        {
+            case "1":
+                SetWeapon(1);
+                break;
+            case "2":
+                SetWeapon(2);
+                break;
+            case "3":
+                SetWeapon(3);
+                break;
+            case "4":
+                SetWeapon(4);
+                break;
+            case "5":
+                SetWeapon(5);
+                break;
+            default:
+                return;
+        }
+    }
+    void SetWeapon(int _keyValue)
+    {
+
     }
 }
