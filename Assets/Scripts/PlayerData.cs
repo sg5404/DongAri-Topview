812831b581b48c46a0a10e8716483a4e76e76fe1
zmflqhaos <zmflqhaos123@gmail.com>
2022-06-02 20:18:24 +0900

    stat

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+    int hp;
+    int mp;
+    int atk;
+    float cooltime;
+    int ammo;
+    int max_ammo;
+    float critical_chance;
+    float critical_dmg;
+    float move_speed;
+    float atk_speed;
+    int gold;
+    int life_drain;
+}
