faffc88a5a379a83bdb2e3c6e36bdcb7a4dc103f
rlagmltn <gmltn2015@naver.com>
2022-06-08 18:07:38 +0900

    Edit Scripts

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,19 +1,25 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData
+public class PlayerData : WeaponSet
 {
     int hp;
     int mp;
-    int atk;
+    float atk;
     float cooltime;
+
     int ammo;
     int max_ammo;
+
     float critical_chance;
     float critical_dmg;
     float move_speed;
     float atk_speed;
+
     int gold;
     int life_drain;
+
+    WeaponKind mainWeaponState = WeaponKind.SWORD;
+    WeaponKind subWeaponState = WeaponKind.RIFLE;
 }
--- Assets/Scripts/WeaponSet.cs
+++ Assets/Scripts/WeaponSet.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public enum WeaponKind
 {
     SWORD = 0,
@@ -12,35 +10,31 @@ public enum WeaponKind
     SHOTGUN,
     GRANADE
 }
-public class WeaponSet : MonoBehaviour
+public class WeaponSet
 {
-
-    private void Update()
+    WeaponKind mainWeaponState = 0;
+    WeaponKind subWeaponState;
+    void SetWeapon(string _input)
     {
-        string input = Input.inputString;
-        switch (input)
+        switch (_input)
         {
             case "1":
-                SetWeapon(1);
+                subWeaponState = WeaponKind.SWORD;
                 break;
             case "2":
-                SetWeapon(2);
+                subWeaponState = WeaponKind.RIFLE;
                 break;
             case "3":
-                SetWeapon(3);
+                subWeaponState = WeaponKind.SNIPER;
                 break;
             case "4":
-                SetWeapon(4);
+                subWeaponState = WeaponKind.SHOTGUN;
                 break;
             case "5":
-                SetWeapon(5);
+                subWeaponState = WeaponKind.GRANADE;
                 break;
             default:
                 return;
         }
-    }
-    void SetWeapon(int _keyValue)
-    {
-
     }
 }
