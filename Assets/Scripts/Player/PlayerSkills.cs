4faff8823d8e811e5b8dd55f6e267d8e77c1b418
냠냠 <lanilani7661@gmail.com>
2022-06-16 20:27:52 +0900

    마당쓸기

--- Assets/Scripts/Player/PlayerSkills.cs
+++ Assets/Scripts/Player/PlayerSkills.cs
@@ -11,6 +11,8 @@ public class PlayerSkills : MonoBehaviour
 
     private List<GameObject> enemyObject;
 
+    private Rigidbody2D rb = null;
+
     private void Awake()
     {
         playerData = GetComponent<PlayerData>();
@@ -36,15 +38,23 @@ public class PlayerSkills : MonoBehaviour
 
         enemyObject = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
 
-        foreach(var enemyItem in enemyObject)
+        foreach (GameObject enemyItem in enemyObject)
         {
-            if(Vector3.Distance(enemyItem.transform.position, transform.position) <= 4f)
+            if(Vector3.Distance(transform.position, enemyItem.transform.position) <= 2f)
             {
-
+                rb = enemyItem.GetComponent<Rigidbody2D>();
+                enemyItem.transform.Rotate(new Vector3(-enemyItem.transform.rotation.x, -enemyItem.transform.rotation.y, -enemyItem.transform.rotation.z));
+                rb.velocity = new Vector2(10f, 10f);
+                Invoke("StopKnockBack", 0.5f);
             }
+
         }
 
         playerAttack.module[playerAttack.weapon].magazine = 8;
     }
 
+    public void StopKnockBack()
+    {
+        rb.velocity = new Vector2(0f, 0f);
+    }
 }
