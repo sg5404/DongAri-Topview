e8798a9fc7b8bc76b4ce5d861c395c871460d6d2
냠냠 <lanilani7661@gmail.com>
2022-07-06 19:45:56 +0900

    TestScene_플레이어 이동 제한구역 설정

--- Assets/Scripts/NavMesh/Move.cs
+++ Assets/Scripts/NavMesh/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     [SerializeField]
     private float speed = 10f;
 
+    Vector3 playerDir = Vector3.zero;
 
     void Update()
     {
@@ -17,8 +18,13 @@ public class Move : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        var dir = new Vector2(h, v).normalized;
+        var dir = new Vector3(h, v, 0).normalized;
 
-        transform.Translate(dir * Time.deltaTime * speed);
+        playerDir += dir * speed * Time.deltaTime;
+
+        playerDir.x = Mathf.Clamp(playerDir.x, -8.5f, 8.5f);
+        playerDir.y = Mathf.Clamp(playerDir.y, -4.5f, 4.5f);
+
+        transform.position = playerDir;
     }
 }
