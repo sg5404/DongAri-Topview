957a7f47afd9fa10d6519982b7281423a5df76f6
sg5404 <77712354+sg5404@users.noreply.github.com>
2022-07-06 20:32:16 +0900

    상점 ui 제작

--- Assets/GUI PRO Kit - Casual Game/Scripts/InventoryScript.cs
+++ Assets/GUI PRO Kit - Casual Game/Scripts/InventoryScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour
 {
@@ -9,11 +10,39 @@ public class InventoryScript : MonoBehaviour
     [SerializeField] private GameObject Content;
     [SerializeField] private GameObject Shop;
     [SerializeField] private bool isShop = false;
-
+    [SerializeField] private bool isSell = false;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private Button sellButton;
     void Start()
     {
         Inventory.SetActive(false);
         Shop.SetActive(false);
+        ResetUI();
+        SetUI();
+    }
+
+    void SetUI()
+    {
+        buyButton.onClick.AddListener(BuyButtonClick);
+        sellButton.onClick.AddListener(SellButtonClick);
+
+        ColorBlock colorBlock = buyButton.colors;
+        colorBlock.normalColor = new Color(0.682353f/1, 0.4588236f/1, 0.2980392f/1, 0);
+        buyButton.colors = colorBlock;
+        buyButton.GetComponentInChildren<Outline>().effectColor = new Color(0, 0, 0, 0);
+
+        colorBlock = sellButton.colors;
+        colorBlock.normalColor = new Color(0.682353f / 1, 0.4588236f / 1, 0.2980392f / 1, 1);
+        sellButton.colors = colorBlock;
+        sellButton.GetComponentInChildren<Outline>().effectColor = new Color(0, 0, 0, 1);
+
+        isSell = false;
+    }
+
+    void ResetUI()
+    {
+        buyButton.onClick.RemoveAllListeners();
+        sellButton.onClick.RemoveAllListeners();
     }
 
     void Update()
@@ -32,5 +61,31 @@ public class InventoryScript : MonoBehaviour
             Shop.SetActive(isShop);
             Content.transform.localPosition = new Vector3(0, 0, 0);
         }
+    }
+
+    void BuyButtonClick()
+    {
+        ColorBlock colorBlock = buyButton.colors;
+        colorBlock.normalColor = new Color(0.682353f / 1, 0.4588236f / 1, 0.2980392f / 1, 0);
+        buyButton.colors = colorBlock;
+        buyButton.GetComponentInChildren<Outline>().effectColor = new Color(0, 0, 0, 0);
+
+        colorBlock = sellButton.colors;
+        colorBlock.normalColor = new Color(0.682353f / 1, 0.4588236f / 1, 0.2980392f / 1, 1);
+        sellButton.colors = colorBlock;
+        sellButton.GetComponentInChildren<Outline>().effectColor = new Color(0, 0, 0, 1);
+    }
+
+    void SellButtonClick()
+    {
+        ColorBlock colorBlock = buyButton.colors;
+        colorBlock.normalColor = new Color(0.682353f / 1, 0.4588236f / 1, 0.2980392f / 1, 1);
+        buyButton.colors = colorBlock;
+        buyButton.GetComponentInChildren<Outline>().effectColor = new Color(0, 0, 0, 1);
+
+        colorBlock = sellButton.colors;
+        colorBlock.normalColor = new Color(0.682353f / 1, 0.4588236f / 1, 0.2980392f / 1, 0);
+        sellButton.colors = colorBlock;
+        sellButton.GetComponentInChildren<Outline>().effectColor = new Color(0, 0, 0, 0);
     }
 }
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    private bool isSell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
